fc8f4dde9c8045b8f5799faabc657900d4963ed7
Özgür Özkan <ozgurozkan123@hotmail.com>
2024-10-04 15:31:19 +0100

    change to min version

--- terraform.tf
+++ terraform.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "1.0.0"
+      version = "2.77.0"
       configuration_aliases = [
         azurerm.management,
         azurerm.connectivity,
